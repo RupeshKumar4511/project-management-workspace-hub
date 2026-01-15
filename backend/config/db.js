9b586b5bc50b1cd81429fc455d3f7f6aa25d9c8e
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-15 19:42:12 +0530

    drizzle config updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,12 @@
 import {Pool} from 'pg';
 import {config} from 'dotenv';
+import {drizzle} from 'drizzle-orm/node-postgres';
 config()
 
 const client = new Pool({
     connectionString:process.env.PostgreSQL_URI
 })
+
+export const db = drizzle(client);
 
 export default client;
