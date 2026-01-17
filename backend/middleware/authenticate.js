9a676dad6be0aa1f02475e382252ce2d4adc0592
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-17 21:23:43 +0530

    authentication middleware updated

--- backend/middleware/authenticate.js
+++ backend/middleware/authenticate.js
@@ -1,14 +1,19 @@
 import jwt from 'jsonwebtoken';
 import {config} from 'dotenv';
 config()
-export const authentication = async(req,res)=>{
+export const authentication = async(req,res,next)=>{
     const accessToken = req.cookies?.accessToken || req.body.accessToken;
+    if(!accessToken){
+        return res.status(401).send({success:false,message:"Access Token is required"})
+    }
 
     try {
         const decodedData = jwt.verify(accessToken,process.env.JWT_SECRET);
         req.user = decodedData;
+
+        return next()
         
     } catch (error) {
-        return res.status(500).send({success:false,message:"internal server error"})
+        return res.status(401).send({success:false,message:"Your access token is expired or invalid"})
     }
 }
