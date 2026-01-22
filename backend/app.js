e66218506737f3e6881911ac4eefd5dea5bd57da
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-22 22:58:23 +0530

    userRoutes added to app

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,21 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import userRoutes from './routes/user.routes.js'
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:'http:localhost:5173/',
+    credentials:true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}))
 
+
+app.use('/api/v1/users/',userRoutes)
 
 
 export default app;
