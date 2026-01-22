251ac42fcf8746d2223d22087f2b02a067113406
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-22 23:00:56 +0530

    verify-email reset-password send-email route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import {generateNewRefreshToken, login,logOut,signUp} from '../controllers/user.controller.js'
-import {loginSchema,signUpSchema} from '../utils/user.validationSchema.js'
+import {generateNewRefreshToken, login,logOut,resetPassword,signUp, verifyEmail} from '../controllers/user.controller.js'
+import {loginSchema,resetPasswordSchema,signUpSchema, verifyEmailSchema} from '../utils/user.validationSchema.js'
 import { checkSchema } from 'express-validator';
 import { userValidation } from '../middleware/user.validation.js';
+import sendOtp from '../services/mail.js'
 
 
 const router = express.Router();
@@ -11,3 +12,11 @@ router.route('/login').post(checkSchema(loginSchema),userValidation,login)
 router.route('/signup').post(checkSchema(signUpSchema),userValidation,signUp)
 router.route('/refresh').post(generateNewRefreshToken)
 router.route('/logout').post(logOut)
+router.route('/verify-email').post(checkSchema(verifyEmailSchema),userValidation,verifyEmail)
+router.route('/reset-password').post(checkSchema(resetPasswordSchema),userValidation,resetPassword)
+
+router.route('/send-email').post(sendOtp,(req,res)=>{
+    return res.status(200).send({success:true,message:"OTP sent successfully"})
+})
+
+export default router;
