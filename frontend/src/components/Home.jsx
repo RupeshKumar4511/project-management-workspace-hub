fe5c6b413c266e260d4570063059c29ad5e8e424
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-24 22:33:11 +0530

    css updated

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     const [isOpenSignIn,setOpenSignIn] = useState(false);
     return (
         <>
-            <header className="flex min-w-full md:py-4 h-12 md:h-auto shadow-md bg-gray-200 items-center px-5 py-1 justify-between">
+            <header className="flex min-w-full md:py-4 h-12 md:h-auto shadow-md bg-gray-200 items-center px-5 py-1 justify-between sticky top-0 z-10">
                 <div className='flex justify-center items-center gap-4'>
                     <h2 className='md:text-3xl text-blue-900 font-bold text-xl'>PMS</h2>
                     <h4 className='pt-1 text-blue-900 text-xs md:text-xl hidden md:block'>Workspace where Companies manage projects</h4>
@@ -17,9 +17,11 @@ const Home = () => {
 
                 <div className="flex gap-2">
                     <button type="button" className={`bg-blue-500 border-blue-600 md:px-4 md:py-1 px-2 py-0.5 md:text-md text-sm rounded-md cursor-pointer text-white ${isOpenSignIn?'hidden': ''}`}
-                     onClick={()=>setOpenSignIn(true)} >Login</button>
+                     onClick={()=>{setOpenSignIn(true); 
+                     window.scrollTo({ top: 0, behavior: "smooth" })}} >Login</button>
                     <button type="button" className={`bg-amber-400 border-amber-600 md:px-4 md:py-1 py-0.5 px-2 md:text-md rounded-md cursor-pointer text-white text-sm ${isOpenSignUp?'hidden': ''}`}
-                    onClick={()=>setOpenSignUp(true)}>Sign-up</button>
+                    onClick={()=>{setOpenSignUp(true); 
+                     window.scrollTo({ top: 0, behavior: "smooth" })}}>Sign-up</button>
                 </div>
             </header>
             <LoginModal isOpen={isOpenSignIn} setOpen={setOpenSignIn}/>
