1c042d34285e97a1d5c782056670843dd558a50f
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-24 22:43:21 +0530

    otp type text

--- backend/models/otp.model.js
+++ backend/models/otp.model.js
@@ -1,9 +1,8 @@
-import { timestamp } from 'drizzle-orm/gel-core'
-import {pgTable,varchar} from 'drizzle-orm/pg-core'
+import {pgTable,varchar,serial,timestamp,text} from 'drizzle-orm/pg-core'
 const otpModel = pgTable("otps",{
     id:serial("id").primaryKey(),
     emailId: varchar("email_Id",{length:255}).notNull(),
-    otp:varchar("otp",{length:6}).notNull(),
+    otp:text("otp").notNull(),
     createdAt:timestamp("created_at").defaultNow().notNull(),
     expiredAt:timestamp("expired_at").notNull()
 })
