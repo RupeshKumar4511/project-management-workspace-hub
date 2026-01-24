27415c80e0b415eeb46e913589460b64bdc0db84
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-24 22:41:53 +0530

    routes updated

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,22 +1,21 @@
 import express from 'express';
-import {generateNewRefreshToken, login,logOut,resetPassword,signUp, verifyEmail} from '../controllers/user.controller.js'
-import {loginSchema,resetPasswordSchema,signUpSchema, verifyEmailSchema} from '../utils/user.validationSchema.js'
+import {generateNewRefreshToken, login,logOut,resetPassword,sendEmailController,signUp, verifyUser} from '../controllers/user.controller.js'
+import {loginSchema,resetPasswordSchema,signUpSchema, verifyUserSchema} from '../utils/user.validationSchema.js'
 import { checkSchema } from 'express-validator';
 import { userValidation } from '../middleware/user.validation.js';
-import sendOtp from '../services/mail.js'
+import verifyEmail from '../middleware/verifyEmail.js';
+import { authentication } from '../middleware/authenticate.js';
 
 
 const router = express.Router();
 
 router.route('/login').post(checkSchema(loginSchema),userValidation,login)
-router.route('/signup').post(checkSchema(signUpSchema),userValidation,signUp)
+router.route('/signup').post(checkSchema(signUpSchema),userValidation,verifyEmail,signUp)
 router.route('/refresh').post(generateNewRefreshToken)
-router.route('/logout').post(logOut)
-router.route('/verify-email').post(checkSchema(verifyEmailSchema),userValidation,verifyEmail)
-router.route('/reset-password').post(checkSchema(resetPasswordSchema),userValidation,resetPassword)
+router.route('/logout').post(authentication,logOut)
+router.route('/verify-user').post(checkSchema(verifyUserSchema),userValidation,verifyUser)
+router.route('/reset-password').post(checkSchema(resetPasswordSchema),userValidation,verifyEmail,resetPassword)
 
-router.route('/send-email').post(sendOtp,(req,res)=>{
-    return res.status(200).send({success:true,message:"OTP sent successfully"})
-})
+router.route('/send-email').post(sendEmailController)
 
 export default router;
