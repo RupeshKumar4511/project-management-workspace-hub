8bf68f286e845f923637a27bf2b4ba8af60defdb
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-24 22:41:03 +0530

    used nodemailer instead of sendgrid

--- backend/services/mail.js
+++ backend/services/mail.js
@@ -1,40 +1,49 @@
 import otpModel from '../models/otp.model.js'
 import { db } from '../config/db.js'
-import bcrypt, { hash } from 'bcrypt'
+import bcrypt from 'bcrypt'
 import { config } from 'dotenv';
+import nodemailer from 'nodemailer'
 config()
-import sgMail from "@sendgrid/mail";
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendOtp = async (req, res, next) => {
-    const { email } = req.body;
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-    try {
-        const otp = Math.floor(100000 + Math.random() * 900000);
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.GMAIL_APP_PASSWORD
+    }
+});
 
-        const mailOptions = {
-            from: process.env.EMAIL,
-            to: email,
-            subject: 'Your OTP Code',
-            text: `Your OTP Code is ${otp}`,
-            html: `<div>
+const sendOtp = async (body) => {
+    const { email } = body;
+
+
+    const otp = Math.floor(100000 + Math.random() * 900000);
+
+    const mailOptions = {
+        from: process.env.EMAIL,
+        to: email,
+        subject: 'Your OTP Code',
+        text: `Your OTP Code is ${otp}`,
+        html: `<div>
             <h2>Welcome to PMS. </h2>
             <h2>Your OTP Code is ${otp}</h2>
         </div>`
 
-        }
-
-        const response = await sgMail.send(mailOptions);
-        console.log(response)
-
-        const hashedOtp = await bcrypt.hash(otp.toString(), 10)
-        const expiresAt = 1000 * 60 * 5; // 5 minute
-        await db.insert(otpModel).values({ emailId: email, otp: hashedOtp, expiredAt: expiresAt })
-
-        return next()
-    } catch (error) {
-        res.status(500).send({success:false,messsage:"Internal Server Error"})
     }
+
+    // const response = await sgMail.send(mailOptions);
+    const response = await transporter.sendMail(mailOptions);
+    console.log(response);
+
+    const hashedOtp = await bcrypt.hash(otp.toString(), 10)
+    const expiresAt = new Date(Date.now() + 1000 * 60 * 5);// 5 minute
+    await db.insert(otpModel).values({ emailId: email, otp: hashedOtp, expiredAt: expiresAt })
+
+
 }
 export default sendOtp;
