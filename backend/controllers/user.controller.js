f865177c22c2a3289670dd92fe75b19894cfba0b
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-24 22:45:18 +0530

    bugs fixed

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,14 +1,14 @@
 import { db } from '../config/db.js';
 import { users, tokens } from '../models/user.model.js'
-import otpModel from '../models/otp.model.js'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
-import { or } from 'drizzle-orm';
+import { eq, or } from 'drizzle-orm';
 import { config } from 'dotenv'
+import sendOtp from '../services/mail.js'
 config()
 
 // generate accessToken and refresh Tokens
-export const generateAccessToken = async (user) => {
+export const generateAccessToken = (user) => {
     return jwt.sign({
         id: user.id,
         username: user.usename,
@@ -30,23 +30,32 @@ export const generateRefreshToken = (user) => {
     )
 }
 
+export const options = {
+            httpOnly: true,
+            secure: true,
+            sameSite: "None"
+        }
 
 //  signup controller 
 export const signUp = async (req, res) => {
     const { username, email, password } = req.body;
 
     try {
-        const [user] = await db.select().from(users).where(or(eq(users.username, username), eq(email, users.email)))
-
+        const [user] = await db.select().from(users).where(or(eq(users.username, username), eq(users.email,email)))
+        
         if (user) {
             return res.status(409).send({ message: "User already exists." })
         }
 
-        await db.insert(users).values({ username, email, password })
-
+        const hashedPassword = await bcrypt.hash(password,10);
+        
+        await db.insert(users).values({ username:username, email:email, password:hashedPassword })
+        
         return res.status(201).send({ success: true, message: "SignUp Successfully" })
+
     } catch (error) {
-        return res.status(500).send({ success: false, message: "internal server error" })
+        console.log(error)
+        return res.status(500).send({ success: false, message: "Internal server error" })
     }
 
 }
@@ -63,29 +72,24 @@ export const login = async (req, res) => {
             return res.status(404).send(errorMsg)
         }
 
-        const isPasswordMatch = bcrypt.compare(password, users.password)
+        const isPasswordMatch = bcrypt.compare(password, user.password)
 
         if (!isPasswordMatch) {
             return res.status(401).send(errorMsg)
         }
 
-        const options = {
-            httpOnly: true,
-            secure: true,
-            sameSite: "None"
-        }
 
-        const accessToken = generateAccessToken(user)
-        const refreshToken = generateRefreshToken(user)
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
 
         const expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7);
 
-        await db.insert(tokens).values({ userId: user.id, refreshToken, expiredAt: expiresAt })
+        await db.insert(tokens).values({ userId: user.id, refreshToken:refreshToken, expiredAt: expiresAt })
 
         res.cookie("accessToken", accessToken, options)
-        res.cookie("refreshToken", refreshToken, options)
+        res.cookie("refreshToken",refreshToken, options)
 
-        return res.status(200).send({ success: true, message: "Login Successfully" })
+        return res.status(200).send({ success: true,username:user.username, message: "Login Successfully" })
 
     } catch (error) {
         return res.status(500).send({ success: false, message: "internal server error" })
@@ -106,7 +110,7 @@ export const generateNewRefreshToken = async (req, res) => {
     try {
 
         const decodedData = jwt.verify(token, process.env.JWT_SECRET)
-        const [dbToken] = await db.select().from(tokens).where(eq(tokens.id, users.userId))
+        const [dbToken] = await db.select().from(tokens).where(eq(tokens.id, decodedData.userId))
 
         if (!dbToken) {
             return res.status(403).send({ message: "Invalid Refresh token" })
@@ -119,10 +123,10 @@ export const generateNewRefreshToken = async (req, res) => {
         const accessToken = generateAccessToken(decodedData)
         const newRefreshToken = generateRefreshToken(decodedData)
 
-        await db.update(tokens).set({ refreshToken: newRefreshToken }).where(eq(token, dbToken.refreshToken))
+        await db.update(tokens).set({ refreshToken: newRefreshToken }).where(eq(dbToken.refreshToken, token))
 
-        res.cookie("accessToken", accessToken)
-        res.cookie("refreshToken", newRefreshToken)
+        res.cookie("accessToken", accessToken,options)
+        res.cookie("refreshToken", newRefreshToken,options)
         res.status(204)
 
     } catch (error) {
@@ -168,21 +172,17 @@ export const logOut = async (req, res) => {
 
 }
 
-export const verifyEmail = async(req,res,next)=>{
-    const {emailId,otp} = req.body;
+export const verifyUser = async(req,res)=>{
+    const {email} = req.body;
 
-    const [data] = db.select({emailId,otp}).from(otpModel).where(eq(emailId,otp.emailId)).orderBy(otpModel.createdBy).limit(1)
+    const [user] = await db.select({email:users.email}).from(users).where(eq(users.email,email));
 
-    if(!data){
+    if(!user){
         res.status(400).send({success:false,message:"Incorrect Email"})
     }
 
-    if(data.otp !== otp){
-        res.status(400).send({success:false,message:"Incorrect OTP"})
-    }
-
-    return res.status(200)
-    // return next()
+    return res.status(200).send({success:true,message:"Email Verified Successfully."})
+    
 
 
 }
@@ -190,7 +190,7 @@ export const verifyEmail = async(req,res,next)=>{
 export const resetPassword = async(req,res)=>{
     const {email,password} = req.body;
 
-    const [user] = db.select({email}).from(users).where(eq(users.email,email));
+    const [user] = await db.select({email}).from(users).where(eq(users.email,email));
 
     if(!user){
         return res.status(401).send({success:false,message:"Unauthorized Request."})
@@ -201,3 +201,17 @@ export const resetPassword = async(req,res)=>{
     return res.status(200).send({success:true,message:"Password reset Successfully"})
     
 }
+
+
+export const sendEmailController = async (req, res) => {
+  try {
+    await sendOtp(req.body);
+    res.status(200).json({
+      success: true,
+      message: "OTP sent successfully",
+    });
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send({success:false,message:"Internal Server Error"})
+  }
+};
