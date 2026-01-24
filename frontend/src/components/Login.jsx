7192f7eca29269d6ff907df081cf33955d6e1c5b
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-24 22:34:16 +0530

    navigate /app

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -24,7 +24,7 @@ export default function Login({ setOpen }) {
  
   useEffect(()=>{
       if (authResponse.success === true) {
-      navigate("/user");
+      navigate("/app");
     }
     },[authResponse,navigate])
 
