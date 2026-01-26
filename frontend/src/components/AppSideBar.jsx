8aade57a8ecd1a7fc2e957b6f58db41ec6293f0f
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-26 22:21:53 +0530

    AppSideBar component

---
+++ frontend/src/components/AppSideBar.jsx
@@ -0,0 +1,59 @@
+import { NavLink, useNavigate } from 'react-router-dom';
+import { IoIosArrowDropdownCircle } from "react-icons/io";
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { signOut } from '../features/authSlice';
+
+
+export default function AppSideBar({ open, setOpen }) {
+    const navigate = useNavigate();
+    const dispatch = useDispatch()
+    const [menu, setMenu] = useState(false);
+
+    const { authResponse, error } = useSelector((store) => store.auth);
+    const handleSignOut = () => {
+        dispatch(signOut({ username: authResponse.username }))
+
+    }
+
+    useEffect(() => {
+        if (authResponse.logout) {
+            setTimeout(()=>{
+                navigate('/');
+            },1000)
+        }
+    }, [authResponse, navigate])
+
+    if (error.signOutError) {
+        alert("Something went wrong.")
+    }
+
+    function getUserName(user) {
+        const username = user.charAt(0).toUpperCase() + user.slice(1);
+        return username;
+    }
+
+
+
+
+    if (authResponse.username) {
+        return (
+
+            <div className={`max-w-max bg-slate-200 flex-col 
+            md:px-6 lg:px-14 px-5 py-4 md:flex fixed top-10 left-0 z-10 md:top-16
+            bottom-0 ${open ? 'flex' : 'hidden'}`}>
+                <NavLink to="/app" className="py-1 lg:text-xl" onClick={() => setOpen(!open)}>Home</NavLink>
+                <NavLink to="/app/create-workspace" className="py-1 lg:text-xl" onClick={() => setOpen(!open)}>Workspaces</NavLink>
+                <div className="py-1 flex lg:text-xl" onClick={() => setMenu((prevState) => !prevState)}>{getUserName(authResponse.username)}<IoIosArrowDropdownCircle className='relative top-2' size={15} /></div>
+                <ul className={`px-2 ${menu ? '' : 'hidden'} md:text-md text-sm`}>
+                    <li><NavLink to="/app/profile" onClick={() => setOpen(!open)}>Profile</NavLink></li>
+                    <li className='cursor-pointer' onClick={() => {
+                        handleSignOut()
+                    }}>Sign out</li>
+                </ul>
+            </div>
+
+        );
+    }
+
+}
