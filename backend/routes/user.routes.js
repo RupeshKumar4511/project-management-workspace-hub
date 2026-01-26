f5084831b8c26ee3fdac84ee2ca7503bb5350a4b
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-26 22:26:59 +0530

    user logout route updated

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,7 +4,6 @@ import {loginSchema,resetPasswordSchema,signUpSchema, verifyUserSchema} from '..
 import { checkSchema } from 'express-validator';
 import { userValidation } from '../middleware/user.validation.js';
 import verifyEmail from '../middleware/verifyEmail.js';
-import { authentication } from '../middleware/authenticate.js';
 
 
 const router = express.Router();
@@ -12,7 +11,7 @@ const router = express.Router();
 router.route('/login').post(checkSchema(loginSchema),userValidation,login)
 router.route('/signup').post(checkSchema(signUpSchema),userValidation,verifyEmail,signUp)
 router.route('/refresh').post(generateNewRefreshToken)
-router.route('/logout').post(authentication,logOut)
+router.route('/logout').post(logOut)
 router.route('/verify-user').post(checkSchema(verifyUserSchema),userValidation,verifyUser)
 router.route('/reset-password').post(checkSchema(resetPasswordSchema),userValidation,verifyEmail,resetPassword)
 
