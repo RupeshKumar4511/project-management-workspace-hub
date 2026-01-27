c3ce43b83234266aa437ada0d843c38387f93713
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-27 23:51:27 +0530

    workspace schema added

---
+++ backend/utils/workspace.validationSchema.js
@@ -0,0 +1,68 @@
+export const workspaceSchema = {
+    workspaceName:{
+        notEmpty:{
+            errorMsg:"Workspace name is required"
+        },
+        isString:{
+            errorMsg:"Workspace name must be string"
+        }
+    },
+    adminEmail:{
+        notEmpty:{
+            errorMsg:"Admin Email is required"
+        },
+        isString:{
+            errorMsg:"Admin Email must be string"
+        },
+        isLength:{
+            options:{
+                max:255,
+                min:8
+            },
+            errorMsg:"Admin Email should be 8-255 characters long."
+        },
+        matches: {
+            options: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/],
+            errorMsg: "Admin email is not valid."
+        }
+    },
+    description:{
+        notEmpty:{
+            errorMsg:"Description is required"
+        },
+        isString:{
+            errorMsg:"Description must be string"
+        }
+    },
+    workspacePassword:{
+        notEmpty:{
+            errorMsg:"workspacePassword is required"
+        },
+        isString:{
+            errorMsg:"workspacePassword must be string"
+        },
+        matches:{
+            options:[/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/],
+            errorMsg: "Password must contain uppercase, lowercase, number & special character"
+        }
+    },
+    adminPassword:{
+        notEmpty:{
+            errorMsg:"workspacePassword is required"
+        },
+        isString:{
+            errorMsg:"workspacePassword must be string"
+        },
+        isLength: {
+            options: {
+                min: 8,
+                max: 32
+            },
+            errorMsg: "Workspace password must be atleast 8-32 characters long."
+        },
+        matches:{
+            options:[/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/],
+            errorMsg: "Workspace password must contain uppercase, lowercase, number & special character"
+        }
+    }
+}
