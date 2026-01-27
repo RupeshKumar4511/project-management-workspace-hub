2e3940aefacaebb2b8441289dbcc210a69dde41c
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-27 23:53:10 +0530

    userValidation renamed as schemaValidation

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,18 +2,18 @@ import express from 'express';
 import {generateNewRefreshToken, login,logOut,resetPassword,sendEmailController,signUp, verifyUser} from '../controllers/user.controller.js'
 import {loginSchema,resetPasswordSchema,signUpSchema, verifyUserSchema} from '../utils/user.validationSchema.js'
 import { checkSchema } from 'express-validator';
-import { userValidation } from '../middleware/user.validation.js';
+import { schemaValidation } from '../middleware/schema.validation.js';
 import verifyEmail from '../middleware/verifyEmail.js';
 
 
 const router = express.Router();
 
-router.route('/login').post(checkSchema(loginSchema),userValidation,login)
-router.route('/signup').post(checkSchema(signUpSchema),userValidation,verifyEmail,signUp)
+router.route('/login').post(checkSchema(loginSchema),schemaValidation,login)
+router.route('/signup').post(checkSchema(signUpSchema),schemaValidation,verifyEmail,signUp)
 router.route('/refresh').post(generateNewRefreshToken)
 router.route('/logout').post(logOut)
-router.route('/verify-user').post(checkSchema(verifyUserSchema),userValidation,verifyUser)
-router.route('/reset-password').post(checkSchema(resetPasswordSchema),userValidation,verifyEmail,resetPassword)
+router.route('/verify-user').post(checkSchema(verifyUserSchema),schemaValidation,verifyUser)
+router.route('/reset-password').post(checkSchema(resetPasswordSchema),schemaValidation,verifyEmail,resetPassword)
 
 router.route('/send-email').post(sendEmailController)
 
