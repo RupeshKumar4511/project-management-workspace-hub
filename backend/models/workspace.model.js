f85acaac0c0ecc8af9c4397d42c17c7beac36dbf
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-27 23:54:03 +0530

    workspace model updated

--- backend/models/workspace.model.js
+++ backend/models/workspace.model.js
@@ -1,12 +1,13 @@
-import { serial, varchar } from "drizzle-orm/pg-core";
-import { pgTable } from "drizzle-orm/pg-core";
-import { timestamp } from "drizzle-orm/pg-core";
+import { serial, varchar,timestamp, pgTable } from "drizzle-orm/pg-core";
+import { users,roleEnum } from "./user.model.js";
+import { text } from "drizzle-orm/gel-core";
 
 export const workspaces = pgTable("workspaces",{
     id:serial("id").primaryKey().notNull(),
-    name:varchar("company_name",{length:20}).notNull(),
+    workspaceName:varchar("workspace_name",{length:20}).notNull().unique(),
     description:varchar("description",{length:255}).notNull(),
-    adminName:varchar("admin_name",{length:32}).notNull(),
+    workspacePassword:text("workspace_password").notNull(),
+    adminEmail:varchar("admin_email",{length:255}).notNull().references(()=>users.email),
     adminPassword:text("admin_password").notNull(),
     createdAt:timestamp('created_at').defaultNow().notNull(),
 })
