42598b639b9f763c59f9762f3edd6d84d8fa6a54
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-27 23:50:05 +0530

    workspace routes added to app

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import userRoutes from './routes/user.routes.js'
+import workspaceRoutes from './routes/workspace.routes.js'
 
 const app = express();
 
@@ -16,7 +17,8 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}))
 
 
-app.use('/api/v1/users',userRoutes)
+app.use('/api/v1/users',userRoutes);
+app.use('/api/v1/workspace',workspaceRoutes);
 
 
 export default app;
