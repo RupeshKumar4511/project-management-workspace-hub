72bde61dba47a414ab42b5dc97ef97510d6b9f77
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-28 00:00:19 +0530

    workspace NavLink updated

--- frontend/src/components/AppSideBar.jsx
+++ frontend/src/components/AppSideBar.jsx
@@ -18,9 +18,7 @@ export default function AppSideBar({ open, setOpen }) {
 
     useEffect(() => {
         if (authResponse.logout) {
-            setTimeout(()=>{
                 navigate('/');
-            },1000)
         }
     }, [authResponse, navigate])
 
@@ -43,7 +41,7 @@ export default function AppSideBar({ open, setOpen }) {
             md:px-6 lg:px-14 px-5 py-4 md:flex fixed top-10 left-0 z-10 md:top-16
             bottom-0 ${open ? 'flex' : 'hidden'}`}>
                 <NavLink to="/app" className="py-1 lg:text-xl" onClick={() => setOpen(!open)}>Home</NavLink>
-                <NavLink to="/app/create-workspace" className="py-1 lg:text-xl" onClick={() => setOpen(!open)}>Workspaces</NavLink>
+                <NavLink to="/app/create-workspace" className="py-1 lg:text-xl" onClick={() => setOpen(!open)}>Workspace</NavLink>
                 <div className="py-1 flex lg:text-xl" onClick={() => setMenu((prevState) => !prevState)}>{getUserName(authResponse.username)}<IoIosArrowDropdownCircle className='relative top-2' size={15} /></div>
                 <ul className={`px-2 ${menu ? '' : 'hidden'} md:text-md text-sm`}>
                     <li><NavLink to="/app/profile" onClick={() => setOpen(!open)}>Profile</NavLink></li>
