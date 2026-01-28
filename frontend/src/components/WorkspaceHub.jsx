a0b6907aed70e15e0e200bf74a79eedb334f8fd4
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-28 23:53:38 +0530

    css updated

--- frontend/src/components/WorkspaceHub.jsx
+++ frontend/src/components/WorkspaceHub.jsx
@@ -12,7 +12,7 @@ export default function WorkspaceHub() {
       <Header setOpen={setOpen}  />
       <div className="min-h-screen flex justify-center bg-gradient-to-br from-slate-50 to-indigo-100 p-6">
         <AppSideBar open={open} setOpen={setOpen}/>
-        <div className="flex justify-center items-center flex-col  ml-40">
+        <div className="flex justify-center items-center flex-col  ml-40 w-full">
           <Outlet/>
         </div>
       </div>
