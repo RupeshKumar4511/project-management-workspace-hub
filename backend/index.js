8feaead21498033f4d5d49459fc658bdb2959d3c
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-01-30 23:42:26 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -18,5 +18,5 @@ const PORT = process.env.PORT || 3000;
 })();
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
