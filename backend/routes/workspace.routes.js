4d4656ba071f79e6c57232e9aa74719cdc80a58b
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-02-07 00:08:22 +0530

    join-workspace route added

--- backend/routes/workspace.routes.js
+++ backend/routes/workspace.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import { createWorkspace } from '../controllers/workspace.controller.js';
+import { createWorkspace,joinWorkspace } from '../controllers/workspace.controller.js';
 import { checkSchema } from 'express-validator';
-import { workspaceSchema } from '../utils/workspace.validationSchema.js';
+import { workspaceSchema,joinWorkspaceSchema } from '../utils/workspace.validationSchema.js';
 import {schemaValidation} from '../middleware/schema.validation.js'
 import {authentication} from '../middleware/authenticate.js'
 
 const router = express.Router();
 
 router.route('/create-workspace').post(checkSchema(workspaceSchema),schemaValidation,authentication,createWorkspace)
+router.route('/join-workspace').post(checkSchema(joinWorkspaceSchema),schemaValidation,authentication,joinWorkspace);
 
 
 
