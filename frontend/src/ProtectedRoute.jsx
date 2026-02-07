3c663baf0a04ba4cedaab2d3c1f7d17a886df795
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-02-08 00:07:52 +0530

    ProtectedRoute Component

---
+++ frontend/src/ProtectedRoute.jsx
@@ -0,0 +1,19 @@
+import {useEffect} from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+    const { authResponse } = useSelector((store) => store.auth);
+    const navigate = useNavigate();
+    
+    useEffect(()=>{
+        if(!authResponse.success) { navigate('/')} 
+    },[authResponse, navigate])
+
+    return (
+        children
+    )
+
+}
+
+export default ProtectedRoute;
