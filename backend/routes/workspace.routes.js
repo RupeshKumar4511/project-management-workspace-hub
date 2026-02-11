6512467313da80899e4eb115b87186ee346e9b3c
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-02-11 13:09:12 +0530

    create-project route implemented

--- backend/routes/workspace.routes.js
+++ backend/routes/workspace.routes.js
@@ -1,14 +1,17 @@
 import express from 'express';
-import { createWorkspace,joinWorkspace } from '../controllers/workspace.controller.js';
+import { createWorkspace,joinWorkspace,createProject } from '../controllers/workspace.controller.js';
 import { checkSchema } from 'express-validator';
-import { workspaceSchema,joinWorkspaceSchema } from '../utils/workspace.validationSchema.js';
+import { workspaceSchema,joinWorkspaceSchema,createProjectSchema } from '../utils/workspace.validationSchema.js';
 import {schemaValidation} from '../middleware/schema.validation.js'
 import {authentication} from '../middleware/authenticate.js'
+import ensureWorkspaceUser from '../middleware/ensureWorkspaceUser.js';
+import restrictTo from '../middleware/restrictTo.js';
 
 const router = express.Router();
 
 router.route('/create-workspace').post(checkSchema(workspaceSchema),schemaValidation,authentication,createWorkspace)
-router.route('/join-workspace').post(checkSchema(joinWorkspaceSchema),schemaValidation,authentication,joinWorkspace);
+router.route('/join-workspace').post(checkSchema(joinWorkspaceSchema),schemaValidation,authentication,ensureWorkspaceUser,joinWorkspace);
+router.route('/create-project').post(checkSchema(createProjectSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["admin"]),createProject)
 
 
 
