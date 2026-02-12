5c26aff8bd9d25d2615afb7ac046b80e1db1f89b
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-02-12 16:55:15 +0530

    workspace model updated

--- backend/models/workspace.model.js
+++ backend/models/workspace.model.js
@@ -1,7 +1,7 @@
-import { serial, varchar,timestamp, pgTable,text, pgEnum } from "drizzle-orm/pg-core";
+import { serial, varchar,timestamp, pgTable,text, pgEnum,integer } from "drizzle-orm/pg-core";
 import { users } from "./user.model.js";
 
-const workspaceRoleEnum = pgEnum("workspace_user_role",["admin","member"])
+export const workspaceRoleEnum = pgEnum("workspace_user_role",["admin","member"])
 export const taskStatusEnum = pgEnum('task_status',['To Do','In Progress','Done'])
 export const projectStatusEnum = pgEnum('project_status',['Planning','Active','Completed','On Hold','Cancelled'])
 export const taskTypeEnum = pgEnum('task_type',['Task','Feature','Bug','Improvement','Other'])
@@ -10,27 +10,28 @@ export const priorityEnum = pgEnum('priority',['Medium','Low','High'])
 
 export const workspaces = pgTable("workspaces",{
     id:serial("id").primaryKey().notNull(),
+    image:varchar("image",{length:1500}).notNull(),
     workspaceName:varchar("workspace_name",{length:32}).notNull().unique(),
     description:varchar("description",{length:255}).notNull(),
     workspacePassword:text("workspace_password").notNull(),
     createdBy:varchar("created_by",{length:32}).references(()=>users.username,{onDelete:'cascade',onUpdate:'cascade'}),
     createdAt:timestamp('created_at').defaultNow().notNull(),
+    updatedAt:timestamp('updated_at').notNull().defaultNow()
 })
-
 
 export const workspaceUsers = pgTable('workspace_users',{
     id:serial("id").notNull().primaryKey(),
-    workspaceName:varchar("workspace_name",{length:32}).notNull().references(()=>workspaces.workspaceName,{onDelete:'cascade'}),
-    username:varchar("username",{length:32}).unique().notNull().references(()=>users.username,{onDelete:'cascade',onUpdate:'cascade'}),
-    email:varchar("email",{length:255}).notNull().unique().references(()=>users.email,{onDelete:'cascade'}),
-    role:workspaceRoleEnum('role').default('member').notNull()
+    workspaceName:varchar("workspace_name",{length:32}).notNull().references(()=>workspaces.workspaceName,{onDelete:'cascade',onUpdate:'cascade'}),
+    username:varchar("username",{length:32}).notNull().references(()=>users.username,{onDelete:'cascade',onUpdate:'cascade'}),
+    role:workspaceRoleEnum('role').default('member').notNull(),
+    createdAt:timestamp("created_at").notNull().defaultNow(),
+    updatedAt:timestamp('updated_at').notNull().defaultNow()
 })
-
 
 export const projects = pgTable('projects',{
     id:serial("id").notNull().primaryKey(),
     title:varchar("title",{length:255}).notNull(),
-    workspaceName:varchar("workspace_name",{length:32}).notNull().references(()=>workspaces.workspaceName,{onDelete:'cascade'}),
+    workspaceName:varchar("workspace_name",{length:32}).notNull().references(()=>workspaces.workspaceName,{onDelete:'cascade',onUpdate:'cascade'}),
     projectLink: text("project_link"),
     description:varchar("description",{length:1000}).notNull(),
     status:projectStatusEnum('status').notNull(),
@@ -38,12 +39,15 @@ export const projects = pgTable('projects',{
     projectLead:varchar("project_lead",{length:32}).notNull().references(()=>workspaceUsers.username,{onDelete:'cascade',onUpdate:'cascade'}),
     startDate:timestamp('start_date').defaultNow(),
     endDate:timestamp('end_date').notNull(),
+    progress:integer("progress",{max:100,min:0}).default(0),
+    createdAt:timestamp("created_at").notNull().defaultNow(),
+    updatedAt:timestamp('updated_at').notNull().defaultNow()
 })
 
 export const projectMembers = pgTable('project_members',{
     id:serial("id").notNull().primaryKey(),
-    username:varchar("username",{length:32}).notNull().references(()=>workspaceUsers.username,{onDelete:'cascade',onUpdate:'cascade'}),
-    projectId:serial("project_id").notNull().references(()=>projects.id,{onDelete:'cascade'}),
+    username:varchar("workspace_username",{length:32}).notNull().references(()=>workspaceUsers.username,{onDelete:'cascade',onUpdate:'cascade'}),
+    projectId:integer("project_id").notNull().references(()=>projects.id,{onDelete:'cascade'}),
 
 })
 
@@ -57,5 +61,7 @@ export const tasks = pgTable('tasks',{
     status:taskStatusEnum("status").notNull(),
     assignee:varchar("assignee",{length:32}).notNull().references(()=>workspaceUsers.username,{onDelete:'cascade',onUpdate:'cascade'}),
     dueDate:timestamp('due_date').notNull(),
+    createdAt:timestamp("created_at").notNull().defaultNow(),
+    updatedAt:timestamp('updated_at').notNull().defaultNow()
     
 })
