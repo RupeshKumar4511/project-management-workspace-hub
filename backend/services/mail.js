b9dce16b61088c532b2e36a5d6aa91d84fa06a48
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-03-01 18:22:19 +0530

    sendMemberInvitation-and-sendTaskInvitation

--- backend/services/mail.js
+++ backend/services/mail.js
@@ -30,7 +30,7 @@ const sendOtp = async (body) => {
         subject: 'Your OTP Code',
         text: `Your OTP Code is ${otp}`,
         html: `<div>
-            <h2>Welcome to PMS. </h2>
+            <h2>Welcome to Workspace Hub </h2>
             <h2>Your OTP Code is ${otp}</h2>
         </div>`
 
@@ -46,4 +46,65 @@ const sendOtp = async (body) => {
 
 
 }
+
 export default sendOtp;
+
+
+export const sendMemberInvitation = async (body) => {
+    const { email, workspaceName, username, password } = body;
+
+    // for existing user 
+    let mailOptions;
+    if (!username && !password) {
+        mailOptions = {
+            from: process.env.EMAIL,
+            to: email,
+            subject: `Invitation from ${workspaceName}`,
+            html: `<div>
+            <h2>Welcome to Workspace Hub </h2>
+            <p>You can join ${workspaceName} by using your own email and password : </p>
+        </div>`
+
+        }
+    }
+
+    // for new user
+    mailOptions = {
+        from: process.env.EMAIL,
+        to: email,
+        subject: `Member Invitation from ${workspaceName}`,
+        html: `<div>
+            <h2>Welcome to Workspace Hub </h2>
+            <p>You can join ${workspaceName} by using following credentials : </p>
+            <h3>Username is ${username}</h3>
+            <h3>Password is ${password}</h3>
+            <strong>You are requested to please reset your username and password for security purpose.</strong>
+        </div>`
+
+    }
+
+    const response = await transporter.sendMail(mailOptions);
+    console.log(response);
+
+}
+
+export const sendTaskInvitation = async (body) => {
+    const { email, title, description } = body;
+
+    const mailOptions = {
+        from: process.env.EMAIL,
+        to: email,
+        subject: `Task Invitation from ${workspaceName}`,
+        html: `<div>
+            <p>You have assigned a new task </p>
+            <h3>Title ${title}</h3>
+            <h3>Description ${description}</h3>
+        </div>`
+
+    }
+
+    const response = await transporter.sendMail(mailOptions);
+    console.log(response);
+
+
+}
