42f4003627b5ec08a9abc89cf89a62acd4bbb03e
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-03-07 20:51:00 +0530

    ensureWorkspaceUser updated

--- backend/middleware/ensureWorkspaceUser.js
+++ backend/middleware/ensureWorkspaceUser.js
@@ -3,16 +3,17 @@ import { db } from "../config/db.js";
 import { workspaceUsers } from "../models/workspace.model.js";
 
 const ensureWorkspaceUser = async (req, res, next) => {
-    const {workspaceName} = req.body;
+    
     try {
-        const [workspaceUser] = await db.select({ email: workspaceUsers.email,role:workspaceUsers.role , workspaceName: workspaceUsers.workspaceName}).from(workspaceUsers).where(and(eq(workspaceUsers.email, req.user.email), eq(workspaceUsers.workspaceName, workspaceName)));
+        const [workspaceUser] = await db.select({ userId: workspaceUsers.id,role:workspaceUsers.role , workspaceId: workspaceUsers.workspaceId}).from(workspaceUsers).where(eq(workspaceUsers.userId, req.user.id));
 
         if (!workspaceUser) {
             return res.status(401).send({ success: false, message: `Unauthorized Request` })
         }
 
         req.user.role = workspaceUser.role;
-        req.user.workspaceName = workspaceUser.workspaceName;
+        req.user.workspaceId = workspaceUser.workspaceId;
+        req.user.userId = workspaceUser.userId
         
         next()
 
